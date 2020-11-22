2b26b5fefa5c0ffe01e71774ebf615d2a0f906cb
Bandin <bandinth@gmail.com>
2020-11-22 13:23:43 +0200

    Edit

---
+++ UnitTestProjectVS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectVS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectVS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa55bede-4950-413b-8d46-9a30defae770")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectVS/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task;
+
+namespace UnitTestProjectVS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add1_Task()
+        {
+            Task.Tasks tasks = new Tasks();
+
+            Task.Task task = new Task.Task(1, "Task1", "Desc", new DateTime(2020, 11, 19), new DateTime(2020, 11, 20), State.Open);
+
+            tasks.AddTask(task);
+
+            Assert.AreEqual(1, tasks.SelectAll().Count);
+
+        }
+
+        [TestMethod]
+        public void Remove1_Task()
+        {
+            Task.Tasks tasks = new Tasks();
+
+
+
+
+        }
+    }
+}
