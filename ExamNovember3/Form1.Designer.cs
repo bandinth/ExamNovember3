52fda2210df78da103274faf08ece5b92fa44a2a
Bandin <bandinth@gmail.com>
2020-11-23 11:34:20 +0200

    Edit

--- ExamNovember3/Form1.Designer.cs
+++ ExamNovember3/Form1.Designer.cs
@@ -31,12 +31,28 @@
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonRemove = new System.Windows.Forms.Button();
             this.buttonEdit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(13, 12);
+            this.buttonAdd.Location = new System.Drawing.Point(12, 342);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(85, 23);
             this.buttonAdd.TabIndex = 0;
@@ -46,43 +62,192 @@
             // 
             // buttonRemove
             // 
-            this.buttonRemove.Location = new System.Drawing.Point(12, 41);
+            this.buttonRemove.Location = new System.Drawing.Point(801, 342);
             this.buttonRemove.Name = "buttonRemove";
             this.buttonRemove.Size = new System.Drawing.Size(85, 23);
             this.buttonRemove.TabIndex = 1;
             this.buttonRemove.Text = "Remove";
             this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
             // 
             // buttonEdit
             // 
-            this.buttonEdit.Location = new System.Drawing.Point(12, 70);
+            this.buttonEdit.Location = new System.Drawing.Point(710, 342);
             this.buttonEdit.Name = "buttonEdit";
             this.buttonEdit.Size = new System.Drawing.Size(85, 23);
             this.buttonEdit.TabIndex = 2;
             this.buttonEdit.Text = "Edit";
             this.buttonEdit.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(233, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(40, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(139, 98);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(32, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(213, 98);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(32, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "РР";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ММ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ДД";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "РР";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(41, 127);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(57, 22);
+            this.textBox7.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(103, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "ММ";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(139, 125);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(32, 22);
+            this.textBox8.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(177, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "ДД";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(213, 130);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(32, 22);
+            this.textBox9.TabIndex = 19;
+            // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(180, 12);
+            this.listBox1.Location = new System.Drawing.Point(504, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(279, 324);
-            this.listBox1.TabIndex = 3;
+            this.listBox1.Size = new System.Drawing.Size(382, 308);
+            this.listBox1.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 450);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(887, 379);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonEdit);
             this.Controls.Add(this.buttonRemove);
             this.Controls.Add(this.buttonAdd);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,6 +256,22 @@
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonRemove;
         private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.ListBox listBox1;
     }
 }
--- ExamNovember3/Form1.cs
+++ ExamNovember3/Form1.cs
@@ -9,19 +9,94 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using UnitTestProjectVS;
 using Task;
+using System.Xml.Serialization;
+using System.IO;
+
 namespace ExamNovember3
 {
+   
     public partial class Form1 : Form
     {
+        
+
+       
+
+        public delegate void MyDelegate(object data);
+
         public Form1()
         {
             InitializeComponent();
+            
+            Task.Tasks tasks = DesirializeXML();
+            foreach (Task.TaskT item in tasks.taskList)
+            {
+                listBox1.Items.Add(item);
+            }
         }
+        public void Add(TaskT task)
+        {
 
+        }
+        public Task.Tasks DesirializeXML()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Task.Tasks));
+            using (FileStream fs = new FileStream("Tasks.xml", FileMode.OpenOrCreate))
+            {
+                Task.Tasks tasks=(Task.Tasks)xml.Deserialize(fs);
+                return tasks;
+            }
+        }
+        public void SerializeXMl(Task.Tasks tasks)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Task.Tasks));
+            using (FileStream fs = new FileStream("Tasks.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, tasks);
+            }
+            
+        }
         private void buttonAdd_Click(object sender, EventArgs e)
         {
+
            
-           
+            Task.Tasks tasks = new Tasks();
+            tasks.taskList = new List<TaskT>();
+            tasks.AddTask(new TaskT(Convert.ToInt32(textBox1.Text),textBox2.Text,textBox3.Text,new DateTime(Convert.ToInt32(textBox4.Text),Convert.ToInt32(textBox5.Text),Convert.ToInt32(textBox6.Text)),
+                new DateTime(Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text), Convert.ToInt32(textBox9.Text)),State.Open));
+            
+            foreach (var item in tasks.taskList)
+            {
+
+                listBox1.Items.Add(item);
+
+            }
+            SerializeXMl(tasks);
+
         }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Tasks tasks = new Tasks();
+            
+            foreach(var item in tasks.taskList)
+            {
+                
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+           
+            
+            Task.Tasks tasks = new Tasks();
+            tasks.taskList.Remove(new TaskT(Convert.ToInt32(textBox1.Text), textBox2.Text, textBox3.Text, new DateTime(Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text)),
+                new DateTime(Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text), Convert.ToInt32(textBox9.Text)), State.Open));
+            foreach(var item in tasks.taskList)
+            {
+                listBox1.Items.Remove(item);
+            }
+        }
+
+        
     }
 }
