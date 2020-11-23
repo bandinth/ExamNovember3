0b48a6c6e58ecf6fac54329798c68a63f9f8b95d
Bandin <bandinth@gmail.com>
2020-11-23 09:09:28 +0200

    ghh

--- UnitTestProjectVS/UnitTest1.cs
+++ UnitTestProjectVS/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task;
 
@@ -8,11 +9,11 @@ namespace UnitTestProjectVS
     public class UnitTest1
     {
         [TestMethod]
-        public void Add1_Task(Task.Task task)
+        public void Add1_Task()
         {
             Task.Tasks tasks = new Tasks();
 
-             task = new Task.Task(1, "Task1", "Desc", new DateTime(2020, 11, 19), new DateTime(2020, 11, 20), State.Open);
+            Task.TaskT task = new Task.TaskT(1, "Task1", "Desc", new DateTime(2020, 11, 19), new DateTime(2020, 11, 20), State.Open);
 
             tasks.AddTask(task);
 
@@ -21,13 +22,35 @@ namespace UnitTestProjectVS
         }
 
         [TestMethod]
-        public void Remove1_Task()
+        public void Edit1_Task()
         {
-            Task.Tasks tasks = new Tasks();
+            Task.TaskT task = new TaskT();
 
+            Task.Tasks tasks = new Tasks();
+            tasks.AddTask(new TaskT(1,"Task2","dawd", new DateTime(2020, 11, 19), new DateTime(2020, 11, 20), State.Open));
+
+            task = tasks.taskList.Where(x => x.Id == 1).FirstOrDefault();
+
+            task.Title = "Hello";
+
+            Assert.AreEqual("Hello", tasks.taskList.Where(x => x.Id == 1).FirstOrDefault().Title);
 
 
 
         }
+        [TestMethod]
+        public void Remove1_Task()
+        {
+            Task.Tasks tasks = new Tasks();
+
+            Task.TaskT task = new Task.TaskT(1, "Task1", "Desc", new DateTime(2020, 11, 19), new DateTime(2020, 11, 20), State.Open);
+
+            tasks.RemoveTask(task);
+
+            Assert.AreEqual(0, tasks.SelectAll().Count);
+
+
+        }
+       
     }
 }
